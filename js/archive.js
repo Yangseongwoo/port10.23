19dadd8f6a5ff88ea84bbb81c4af19988e1f15b2
Yangseongwoo <163240206+Yangseongwoo@users.noreply.github.com>
2024-10-23 16:57:20 +0900

    update 4_57

--- js/archive.js
+++ js/archive.js
@@ -2,23 +2,27 @@ document.addEventListener("DOMContentLoaded", function () {
     const cursorImage = document.getElementById("cursor-image");
     const archivesContents = document.querySelectorAll(".archives-content");
 
-    archivesContents.forEach(content => {
-        content.addEventListener("mouseenter", function(event) {
-            cursorImage.style.display = "block"; // 이미지 표시
-            document.body.style.cursor = "none"; // 기본 커서 숨김
-        });
+    // 화면 너비가 768px 이상일 때만 이벤트 리스너 추가
+    if (window.innerWidth > 768) {
+        archivesContents.forEach(content => {
+            content.addEventListener("mouseenter", function(event) {
+                cursorImage.style.display = "block"; // 이미지 표시
+                document.body.style.cursor = "none"; // 기본 커서 숨김
+            });
 
-        content.addEventListener("mousemove", function(event) {
-            cursorImage.style.left = `${event.pageX}px`; // 커서의 X 좌표
-            cursorImage.style.top = `${event.pageY}px`; // 커서의 Y 좌표
-        });
+            content.addEventListener("mousemove", function(event) {
+                cursorImage.style.left = `${event.pageX}px`; // 커서의 X 좌표
+                cursorImage.style.top = `${event.pageY}px`; // 커서의 Y 좌표
+            });
 
-        content.addEventListener("mouseleave", function() {
-            cursorImage.style.display = "none"; // 이미지 숨김
-            document.body.style.cursor = "default"; // 기본 커서로 복원
+            content.addEventListener("mouseleave", function() {
+                cursorImage.style.display = "none"; // 이미지 숨김
+                document.body.style.cursor = "default"; // 기본 커서로 복원
+            });
         });
-    });
+    }
 });
+
 
 
 
